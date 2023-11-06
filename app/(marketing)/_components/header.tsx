1cce055ed563d1c7f41121585105dafc737ad169
YuBin <bin8727@naver.com>
2023-11-06 22:50:56 +0900

    사이드바 설정 후 문서페이지 유저 세팅 중

--- app/(marketing)/_components/header.tsx
+++ app/(marketing)/_components/header.tsx
@@ -14,7 +14,7 @@ const Header = () => {
     <div className="max-w-3xl space-y-4">
       <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
         Your Ideas, Documents, & Plans. Unified. Welcom to{" "}
-        <span className="underline">Notion.</span>
+        <span className="underline">Notion</span>
       </h1>
 
       <h3 className="text-base sm:text-xl md:text-2xl font-medium">
