c9b39a3a28b4048701f5791b244c8e9e7a409062
YuBin <bin8727@naver.com>
2023-11-14 21:49:50 +0900

    Document List 자식 쌓기

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -12,15 +12,15 @@ import { usePathname } from "next/navigation";
 import { ElementRef, useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
 import UserItem from "./userItem";
-import { useMutation, useQuery } from "convex/react";
+import { useMutation } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import Item from "./item";
 import { toast } from "sonner";
+import DocumentList from "./documentList";
 
 const Navigation = () => {
   const pathname = usePathname();
   const isMobile = useMediaQuery("(max-width: 768px)");
-  const documents = useQuery(api.documents.get);
   const create = useMutation(api.documents.create);
 
   const isResizingRef = useRef(false);
@@ -144,9 +144,7 @@ const Navigation = () => {
         </div>
 
         <div className="mt-4">
-          {documents?.map((doc) => (
-            <p key={doc._id}>{doc.title}</p>
-          ))}
+          <DocumentList />
         </div>
 
         <div
